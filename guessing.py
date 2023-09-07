3ca90a138f27756a9f3c756df72b0c95054216ec
Juan R. Rodrigues <juancontato27@gmail.com>
2023-09-07 14:21:54 -0300

    Update guessing.py

--- guessing.py
+++ guessing.py
@@ -6,25 +6,27 @@ Welcome to the Guessing Game
 
 secret_number: int = 42
 totalRounds = 3
-round = 1
 
-while(round <= totalRounds):
+for round in range(1, totalRounds + 1):
     print("Round {} of {}".format(round, totalRounds))
-    number_user = input("Enter Your Number: ")
+    number_user = input("Enter Your Number between 1 and 100: ")
     print("Your Typed: ", number_user)
     number_user = int(number_user)
 
+    if(number_user < 1 or number_user > 100):
+        print("You must enter a number between 1 and 100")
+        continue
     correct = secret_number == number_user
     maior = number_user > secret_number
     menor = number_user < secret_number
 
     if (correct):
         print("Correct")
-        round = 4
+        break
     else:
         if (maior):
             print("Incorrect! Your Number is Greater than the Secret Number")
         elif (menor):
              print("Incorrect! Your number is Less than the Secret Number")
 
-        round = round + 1
+
